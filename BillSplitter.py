0dbd149a3e33ac3b9c9f4605492573657fbb1412
EC2 Default User <ec2-user@ip-172-31-44-100.us-east-2.compute.internal>
2019-07-18 18:16:39 +0000

    initial commit

---
+++ BillSplitter.py
@@ -0,0 +1,18 @@
+# MVP = 
+import math
+
+# subTotal = int(subTotal) 
+total = input("What is the total of your bill?\n")
+people = input("How many people are you dining with? Include yourself.\n")
+totalCost = int(total) / int(people)
+print ("The cost of your bill pre-tips is " + str(totalCost) + ".")
+# tip = input("Would you like to include tip? Yes or No? \n")
+
+# if tip[0].lower() == "y":
+#     chosenTip = input("What percentage tip would you like to give? Example: 15% is 15.\n")
+
+#     elif  
+
+
+# final = math.ceil(totalCostWithTax)
+# print ("Each person must pay $" + ".")
